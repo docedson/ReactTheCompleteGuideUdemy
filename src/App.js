b4f50da0132adbbc88247cd8fc17430098ebc6f6
AndySnapIT <C_Edson.Andrew@cerner.com>
2020-08-10 16:24:50 -0500

    Finished lesson 31: JSX Restrictions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
   }
+}
 
 export default App;
