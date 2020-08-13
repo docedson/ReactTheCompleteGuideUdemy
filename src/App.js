4c62b7ddb7a45d2bfd7f8d447e3f2393095ec18c
AndySnapIT <C_Edson.Andrew@cerner.com>
2020-08-13 12:40:40 -0500

    Finished lesson 54: Handling Dynamic Content 'The JavaScript Way'

--- src/App.js
+++ src/App.js
@@ -41,8 +41,6 @@ class App extends Component {
   }
 
   render () {
-    //// This is in-line styling which can be used, but is limited compared to
-    ///using true css styling
     const style = {
       backgroundColor: 'white',
       font: 'inherit',
@@ -51,6 +49,26 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Max!')}
+            changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div>
+      );
+    }
+
     return (
       <div className='App'>
         <h1>Hi, I'm a React App</h1>
@@ -58,21 +76,7 @@ class App extends Component {
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Switch Name</button>
-          { this.state.showPersons ?
-            <div>
-              <Person
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age} />
-              <Person
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, 'Max!')}
-                changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-              <Person
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age} />
-            </div> : null
-          }
+        {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
